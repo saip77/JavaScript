f3ecfa402944feafaedea089aba1b1f3bd668d37
SAI PRASAD <sp12961369@gmail.com>
2026-01-10 23:29:50 +0530

    Objects

---
+++ Day-03/01-objects.js
@@ -0,0 +1,15 @@
+/* Understanding Objects
+Objects are key-value pairs, where the key is a string and the value can be of any data type.
+*/
+
+// Creating an object
+let person = {
+    name: "Sai Prasad",
+    age: 24,
+    hobbies: ["Coding", "Reading", "Traveling"]
+}
+
+// Accessing properties
+console.log(person.name);
+// Other method
+console.log(person["name"]);
